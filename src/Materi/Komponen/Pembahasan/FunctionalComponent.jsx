403db7e4e14ee1121172c8260d35207e1691b827
yulianto <jooliantoo@gmail.com>
2023-10-25 19:37:38 +0700

    test-1

---
+++ src/Materi/Komponen/Pembahasan/FunctionalComponent.jsx
@@ -0,0 +1,9 @@
+const FunctionalComponent = ({nama}) => {
+    return (
+        <div>
+            <h1>Membuat komponen dengan functional component</h1>
+            <h2>Hallo {nama}, selamat belajar</h2>
+        </div>
+    )
+}
+export default FunctionalComponent;
